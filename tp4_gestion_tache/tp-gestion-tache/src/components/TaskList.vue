450cda15c684313b406227d0d99879b40e097117
fatimaezzahraaelankoud <fatimaezzahraaelankoud@gmail.com>
2025-11-06 14:51:52 +0100

    ajout fichier  json des taches/ composants (task list) avec titre et 3 buttons de filtrage

---
+++ tp4_gestion_tache/tp-gestion-tache/src/components/TaskList.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="task-list">
+    <h2>Liste des tâches</h2>
+    <div class="buttons">
+    <button>toutes les taches </button>
+    <button> taches pas terminees</button>
+    <button> taches terminees</button>
+    </div>
+   
+  
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TaskList",
+  data() {
+    return {
+     
+    };
+  },
+  computed: {
+   
+  },
+  methods: {
+  }
+};
+</script>
+
+<style scoped>
+
+.task-list{
+  border: 1px solid #000000 ;
+  display: flex ;
+  flex-direction: column ;
+  justify-contend: center ;
+  padding: 12px 8% ;
+  width: 40% ;
+  margin: 0% auto ;
+  text-align: center ;
+  border-radius: 8px  ;
+  background-color: #ffffffff
+}
+h2{
+  color: #1f34d3ff
+
+}
+.buttons{
+ 
+  display: flex ;
+  justify-content: space-between 
+
+}
+button{
+ background-color: #1f34d3ff ;
+  color: #ffffff ;
+  padding: 4px ;
+  border: 1px solid #000000 ;
+  border-radius: 8px
+}
+
+</style>
+
