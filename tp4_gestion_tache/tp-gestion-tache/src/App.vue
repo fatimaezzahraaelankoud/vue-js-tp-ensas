60c89ed500d2bf1694c56a9d8b3f5ecb6bee835f
fatimaezzahraaelankoud <fatimaezzahraaelankoud@gmail.com>
2025-11-07 18:08:21 +0100

    fetching data from the file json

--- tp4_gestion_tache/tp-gestion-tache/src/App.vue
+++ tp4_gestion_tache/tp-gestion-tache/src/App.vue
@@ -21,7 +21,7 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  background-color: #d2d4d6ff;
+  background-color: #ffffffff;
   width: 100%
 }
 </style>
--- tp4_gestion_tache/tp-gestion-tache/src/components/TaskList.vue
+++ tp4_gestion_tache/tp-gestion-tache/src/components/TaskList.vue
@@ -2,9 +2,18 @@
   <div class="task-list">
     <h2>Liste des tâches</h2>
     <div class="buttons">
-    <button>toutes les taches </button>
-    <button> taches pas terminees</button>
-    <button> taches terminees</button>
+    <button @click="filter_all">toutes les taches </button>
+    <button @click="filter_all"> taches à faire</button>
+    <button @click="filter_all"> taches terminées</button>
+    </div>
+    <div class="listestasks">
+    <div class="listtache" v-for="tache in taches" :key="tache.id" >
+      <h3>Titre: {{tache.title}}</h3>
+      <div class="description_button">
+      <p>{{tache.description}}</p>
+      <button></button>
+      </div>
+      </div>
     </div>
    
   
@@ -16,14 +25,24 @@ export default {
   name: "TaskList",
   data() {
     return {
-     
+     taches:[]
+  
     };
   },
   computed: {
    
   },
   methods: {
+  async fetchTache(){
+     const res = await fetch("/listtache.json");
+     this.taches = await res.json();
   }
+  
+  },
+  mounted() {
+    this.fetchTache();
+  }
+
 };
 </script>
 
@@ -48,16 +67,51 @@ h2{
 .buttons{
  
   display: flex ;
-  justify-content: space-between 
+  justify-content: space-between ;
+
 
 }
 button{
  background-color: #1f34d3ff ;
   color: #ffffff ;
-  padding: 4px ;
   border: 1px solid #000000 ;
-  border-radius: 8px
+  border-radius: 8px ;
+  cursor: pointer ;
+  width: 32% ;
+  height: 30px 
 }
+
+.listtache{
+width: 90% ;
+display: flex ;
+flex-direction: column ;
+}
+.description_button{
+display: flex ;
+justify-content: center ;
+text-align: center ;
+justify-content: space-around ;
+
+
+}
+.description_button button{
+width: 30px ;
+height: 30px ;
+background-color: #ffffffff ;
+margin-top : 8px
+}
+.listtache h3{
+margin-bottom: -10px
+}
+
+.listestasks{
+border: 1px solid #000000 ;
+border-radius: 8px ;
+margin: 20px auto ;
+width: 100%
+
+}
+
 
 </style>
 
