fc47657df1bb7b590a3629a07cd48b3323832be5
fatimaezzahraaelankoud <fatimaezzahraaelankoud@gmail.com>
2025-11-30 21:10:14 +0100

    ajout tp6/blog

---
+++ tp6/blog-app/src/components/NavBar.vue
@@ -0,0 +1,15 @@
+<template>
+  <nav class="navbar">
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/create">Add Post</RouterLink>
+  </nav>
+</template>
+
+<style>
+.navbar {
+  display: flex;
+  gap: 25px;
+  padding: 15px;
+  background: #f5f5f5;
+}
+</style>
---
+++ tp6/blog-app/src/components/TagCloud.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="tag-cloud">
+    <h3>Tags</h3>
+    <RouterLink
+      v-for="tag in uniqueTags"
+      :key="tag"
+      :to="`/tags/${tag}`"
+    >
+      #{{ tag }}
+    </RouterLink>
+  </div>
+</template>
+
+<script setup>
+import { computed } from "vue";
+const props = defineProps({
+  posts: {
+    type: Array,
+    default: () => [],
+  },
+});
+
+const uniqueTags = computed(() => {
+  const all = props.posts.flatMap((p) => p.tags || []);
+  return [...new Set(all)];
+});
+</script>
+
+<style>
+.tag-cloud {
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+}
+</style>
